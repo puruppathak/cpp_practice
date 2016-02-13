9fcfd0a2d3b15873fefde47fc13a70551aa1bfd2
Puru Pathak <puruppathak@gmail.com>
2016-02-13 16:26:36 -0500

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+//#include <conio>
+//#include <stdio>
+using namespace std;
+using std::cout;
+
+int main()
+{
+
+cout<<"Hello: This is Bubble sort\n";
+int a[4]={2,1,3,4};
+int i;
+cout<<"Array is: \n";
+int temp;
+for(int i=0;i<4;i++)
+{
+
+cout<<a[i]<<"\n";
+
+}
+
+for(i=0;i<4;i++)
+{
+if(a[i]>a[i+1])
+{
+
+temp=a[i];
+a[i]=a[i+1];
+a[i+1]=temp;
+i++;
+}
+}
+cout<<"Sorted array is: "<<"\n";
+
+for(int i=0;i<4;i++)
+{
+
+cout<<a[i]<<"\n";
+
+}
+
+
+return 0;
+
+}
