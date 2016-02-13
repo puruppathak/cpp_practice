6a1dae321a22f2d4bc8f0172fca0b07660aa9b29
Puru Pathak <puruppathak@gmail.com>
2016-02-12 21:24:31 -0500

    Finding if the two strings are anagrams

---
+++ anagram_checker.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm> 
+#include <stdio.h>
+using namespace std;
+using std::cout;
+#include <string.h>
+
+
+int main()
+{
+
+    string str1;
+    string str2;
+    
+    cout<<"Enter first string: \n";
+    cin>>str1;
+    cout<<"Enter second string: \n";
+    cin>>str2;
+    if(str1.length()!=str2.length())
+    {
+        cout<<"Not anagrams \n";
+        return 0;
+    }
+   // cout<<len1;
+    sort(str1.begin(),str1.end());
+    sort(str2.begin(),str2.end());
+    int ret = strcmp(str1.c_str(), str2.c_str());
+    if(ret==0)
+    {
+    	cout<<"Strings are anagrams \n";
+    }
+    else
+    {
+
+    	cout<<"String are not anagrams \n";
+    }
+    
+    
+    return 0;
+}
