24148e57836fe7757eea766b76de425480f4f643
Puru Pathak <puruppathak@gmail.com>
2016-02-16 21:59:17 -0500

    Create Array_Unique.cpp
    Finding unique elements in an array

---
+++ Array_Unique.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+using std::cout;
+
+int main()
+{
+
+int a[200]={};
+int n,i,j;
+cout<<"Enter number of elements: \n";
+cin>>n;
+cout<<"Enter array elements: \n";
+for(i=0;i<n;i++)
+{
+
+cin>>a[i];
+
+}
+
+cout<<"Array entered is: \n";
+
+for(i=0;i<n;i++)
+{
+
+cout<<a[i]<<"\n";
+
+}
+cout<<"Unique elements in this array are: \n";
+for(i=0;i<n;i++)
+{
+  for(j=0;j<i;j++)
+{
+
+   if(a[i]==a[j])
+   {
+
+    break;
+
+   }
+}
+if(i==j)
+{
+
+  cout<<a[i]<<"\n";
+}
+}
+return 0;
+}
