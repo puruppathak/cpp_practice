b9750e91dbff169063293e6b581338ba17882344
Puru Pathak <puruppathak@gmail.com>
2016-02-19 00:54:52 -0500

    Create duplicates.cpp
    Checking if duplicate elements are present in an array.

---
+++ duplicates.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+
+
+bool checkdup(int a[],int n)
+{
+ int i,j;
+
+
+	for( i = 0; i < n; i++ )
+ { //cout<<"In first for loop";
+  for( j = i+1; j < n; j++ )
+  {
+   if( a[i] == a[j] )
+    return true;
+//cout<<"true";
+  }
+ }
+ return false;
+
+}
+
+
+int main()
+{
+
+cout<<"Hello: This is Selection sort\n";
+int a[]={2,1,3,5};
+int i,n,j;
+cout<<"Array is: \n";
+int temp;
+for(int i=0;i<4;i++)
+{
+
+cout<<a[i]<<"\n";
+
+}
+
+bool result=checkdup(a,4);
+cout<<"Result for duplicates is(1=Yes,0=No): "<<result<<"\n";
+
+return 0;
+
+}
+
