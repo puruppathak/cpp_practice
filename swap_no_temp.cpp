d8e7c2771e1b6c23b8e99782f15962c959e4e3f2
Puru Pathak <puruppathak@gmail.com>
2016-02-21 20:32:54 -0500

    Create swap_no_temp.cpp
    Swap two numbers without using temp.

---
+++ swap_no_temp.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+//#include <conio>
+//#include <stdio>
+using namespace std;
+using std::cout;
+#include <math.h>
+
+int main()
+{
+int x1=3;
+int x2=5;
+int diff;
+
+cout<<"Swapping nos without using temp variable: \n";
+cout<<"Numbers before swapping: \n";
+cout<<"First no is: "<<x1<<"\n";
+cout<<"Second no is: "<<x2<<"\n";
+
+if(x1>x2)
+{
+
+
+	diff=x1-x2;
+	x1=x1-diff;
+	x2=x2+diff;
+}
+else
+{
+
+diff=x2-x1;
+
+	x2=x2-diff;
+	x1=x1+diff;
+
+}
+
+cout<<"Swapping nos: \n";
+cout<<"Now, First no is: "<<x1<<"\n";
+cout<<"Now, Second no is: "<<x2<<"\n";
+return 0;
+}
